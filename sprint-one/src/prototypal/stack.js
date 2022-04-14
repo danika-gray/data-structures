4b9049f2b9d5d48c409b811476fd0d8d7a689a57
danika-gray <danika.partridge@gmail.com>
2022-04-14 11:26:54 -0700

    pass all tests in prototypal/stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var stackObj = Object.create(stackMethods);
+  stackObj.storage = {};
+  stackObj.top = 0;
+  stackObj.bottom = 0;
+
+  return stackObj;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+    this.top++;
+    this.storage[this.top] = value;
+  },
+
+  pop: function() {
+    if (this.top - this.bottom > 0) {
+
+      var topValue = this.storage[this.top];
+      delete this.storage[this.top];
+
+
+      this.top--;
+      return topValue;
+    }
+  },
+
+  size: function() {
+    var size = this.top - this.bottom;
+    return size;
+  },
+
+};
 
 
