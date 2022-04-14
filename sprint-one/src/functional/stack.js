668ac75579133f919ccd60fdba1982abea7805b4
danika-gray <danika.partridge@gmail.com>
2022-04-13 17:52:53 -0700

    pass all tests for queue.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,33 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var top = 0;
+  var bottom = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add value to someInstance (to the top)
+    storage[top] = value; // storage['0'] returns value
+    top++;
+    console.log(storage);
   };
 
   someInstance.pop = function() {
+    // remove top value from storage if size is > 0
+    if (top - bottom > 0) {
+      var popValue = storage[top - 1];
+      //console.log('popValue: ' + popValue)
+      delete storage[top];
+
+      top--;
+      return popValue;
+    }
   };
 
   someInstance.size = function() {
+    // size of the stack should be top - bottom
+    var size = top - bottom;
+    return size;
   };
 
   return someInstance;
